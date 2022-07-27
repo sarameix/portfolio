c729f6bd1e6f1b1cc3b22bda23da7f3bb3ac7523
smeixner <sarameix22@gmail.com>
2022-07-27 11:24:46 -0400

    first version of hamburger menu

--- scripts/app_home.js
+++ scripts/app_home.js
@@ -1,1 +1,17 @@
 console.log('jQuery is working!');
+
+// Function to Toggle Hamburger Menu
+const toggleBurger = () => {
+    const $burgerMenuPanel = $('#hamburger-menu-background');
+    if($burgerMenuPanel.is(':visible')){
+        $burgerMenuPanel.hide("slide", {direction: "right"}, 1000);
+    } else {
+        $burgerMenuPanel.show();
+    }
+}
+
+// Wait for HTML to Load to Run This
+$(() => {
+    // Set Up Hamburger Menu Button to Toggle Menu
+    $('#hamburger-menu-button').on('click', toggleBurger);
+})
