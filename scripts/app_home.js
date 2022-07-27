acd8b273ba12f9a0a50a1f29a9b27a9523dd47f5
smeixner <sarameix22@gmail.com>
2022-07-27 12:35:32 -0400

    fixing burger menu display issue

--- scripts/app_home.js
+++ scripts/app_home.js
@@ -11,15 +11,23 @@ let isHiddenMenu = true;
 
 // Function to Toggle Hamburger Menu
 const toggleBurger = () => {
-    // Getting Hamburger Menu Panel Element
+    // Getting Hamburger Menu Panel Element and Width
     const $burgerMenuPanel = $('#hamburger-menu-panel');
     const $burgerMenuWidth = $burgerMenuPanel.width();
+
     // Toggle Menu Panel Based on Global Boolean Variable
     if(isHiddenMenu){ // If Menu Hidden
+        $burgerMenuPanel.css('display', 'block');
         $burgerMenuPanel.animate({'right': 0}, 600, 'swing');
         isHiddenMenu = false;
     } else { // If Menu Visible
-        $burgerMenuPanel.animate({'right': -$burgerMenuWidth}, 450, 'swing');
+        // Found Code for Next 3 Lines on Stack Overflow
+        // Used to Delay Changing Display to Hidden in Panel's CSS
+        // https://stackoverflow.com/questions/5396119/using-jquery-delay-with-css
+        $burgerMenuPanel.animate({'right': -$burgerMenuWidth}, 450, 'swing').delay(450).queue(function (next) { 
+            $burgerMenuPanel.css('display', 'none'); 
+            next();
+        });
         isHiddenMenu = true;
     }
 }
