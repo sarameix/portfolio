9e74bde2fc16615f5827ce15b46102bf34677c6c
smeixner <sarameix22@gmail.com>
2022-07-27 15:01:25 -0400

    the computer fan is going crazy and I'm paranoid I'll lose my progress

--- scripts/app_about.js
+++ scripts/app_about.js
@@ -1,1 +1,52 @@
-console.log('jQuery is working!');
+/////////////////////////////////////
+// * GLOBAL VARIABLE DECLARATION * //
+/////////////////////////////////////
+
+// Variable Tracking if Hamburger Menu is Open
+let isHiddenMenu = true;
+
+//////////////////////////////
+// * FUNCTION DECLARATION * //
+//////////////////////////////
+
+// Function to Toggle Hamburger Menu
+const toggleBurger = () => {
+    // Getting Hamburger Menu Panel Element and Width
+    const $burgerMenuPanel = $('#hamburger-menu-panel');
+    const $burgerMenuWidth = $burgerMenuPanel.width();
+
+    // Toggle Menu Panel Based on Global Boolean Variable
+    if(isHiddenMenu){ // If Menu Hidden
+        // Show Menu Panel
+        $burgerMenuPanel.show(1000)
+
+        // Toggle Buttons
+        $('.hamburger-icon').css('display', 'none');
+        $('#X1').css('display', 'block');
+        $('#X2').css('display', 'block');
+
+        // Change Boolean
+        isHiddenMenu = false;
+    } else { // If Menu Visible
+        // Hide Menu Panel
+        $burgerMenuPanel.hide(750);
+
+        // Toggle Buttons
+        $('.hamburger-icon').css('display', 'block');
+        $('#X1').css('display', 'none');
+        $('#X2').css('display', 'none');
+
+        // Change Boolean
+        isHiddenMenu = true;
+    }
+}
+
+////////////////////////////
+// * EXECUTE AFTER LOAD * //
+////////////////////////////
+
+// Wait for HTML to Load to Run This
+$(() => {
+    // Set Up Hamburger Menu Button to Toggle Menu
+    $('#hamburger-menu-button').on('click', toggleBurger);
+})
