dd467630c361f2ec2aeb8fee0b32e839062ffb2e
smeixner <sarameix22@gmail.com>
2022-07-28 10:48:25 -0400

    adding project overlay on hover jquery functionality to projects page

--- scripts/app_projects.js
+++ scripts/app_projects.js
@@ -41,6 +41,36 @@ const toggleBurger = () => {
     }
 }
 
+// Function to Show Project Overlay
+const showOverlay = (event) => {
+    // Getting Div from Arguments
+    let $projDiv = event.data.div;
+
+    // Show Overlay Div
+    $projDiv.children().eq(2).fadeIn(500);
+
+    // Delay Showing Title and Description
+    setTimeout(function() {
+        $projDiv.children().eq(0).slideToggle(750);
+        $projDiv.children().eq(1).slideToggle(500);
+    }, 175);
+}
+
+// Function to Show Project Overlay
+const hideOverlay = (event) => {
+    // Getting Div from Arguments
+    let $projDiv = event.data.div;
+
+    // Hide Overlay Div
+    $projDiv.children().eq(2).fadeOut(800);
+
+    // Delay Hiding Title and Description
+    setTimeout(function() {
+        $projDiv.children().eq(0).slideToggle(750);
+        $projDiv.children().eq(1).slideToggle(500);
+    }, 75);
+}
+
 ////////////////////////////
 // * EXECUTE AFTER LOAD * //
 ////////////////////////////
@@ -49,4 +79,22 @@ const toggleBurger = () => {
 $(() => {
     // Set Up Hamburger Menu Button to Toggle Menu
     $('#hamburger-menu-button').on('click', toggleBurger);
+
+    // Set Each Project Div Up with Hover Events
+    // Found Documentation for Each Function Here:
+    // https://api.jquery.com/each/
+    $('.project').each(function(){
+        // Show Overlay on Mouse Enter
+        $(this).on(
+            'mouseenter', 
+            {div: $(this)},
+            showOverlay
+        );
+        // Hide Overlay on Mouse Leave
+        $(this).on(
+            'mouseleave', 
+            {div: $(this)},
+            hideOverlay
+        );
+    })
 })
